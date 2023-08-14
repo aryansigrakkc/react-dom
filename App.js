9477429c5aef4d1a2d507f50b3ef6d143b7a56e8
aryansigrakkc <141600997+aryansigrakkc@users.noreply.github.com>
2023-08-14 12:39:48 +0530

    first message

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent  = React.createElement('div',{id:"parent"},
+[
+    React.createElement('div',{id:"child"},[
+        React.createElement('h1',{},"Hello this is the h1 tag"),
+        React.createElement('h2',{},"This is the h2 tag")
+    ]),
+    React.createElement('div',{id:"child2"},[
+        React.createElement('h1',{},"I'm on h1 tag"),
+        React.createElement('h2',{},"I'm on h2 tag")
+    ]),
+])
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
